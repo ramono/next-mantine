d022127701a1102e0aa3c924e1d2e0d345fe3bb6
ramonlapenta <rlapenta@caci.co.uk>
2024-05-09 09:19:12 -0600

    Add stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,52 @@
+'use client';
+
+import { Grid, GridCol, Container, Flex, Title } from '@mantine/core';
+import { HeaderMenu } from '../lib/HeaderMenu/HeaderMenu';
+import { HeroImageRight } from '../lib/HeroImageRight/HeroImageRight';
+import { FeaturesTitle } from '../lib/FeaturesTitle/FeaturesTitle';
+import { StatsRing } from '../lib/StatsRing/StatsRing';
+import { ArticleCardVertical } from '../lib/ArticleCardVertical/ArticleCardVertical';
+import { FooterLinks } from '../lib/FooterLinks/FooterLinks';
+import classes from "./page.module.css";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <div className="wrap">
+        <HeaderMenu />
+
+        <HeroImageRight />
+
+        <Container size="lg">
+            <FeaturesTitle />
+        </Container>
+
+        <div className={classes.statsWrapper}>
+            <Container size="lg">
+                <StatsRing />
+            </Container>
+        </div>
+
+        <div className={classes.newsWrapper}>
+            <Container size="lg">
+                <Grid>
+                    <GridCol span={12}>
+                        <Title order={2}>Latest news</Title>
+                    </GridCol>
+                </Grid>
+            </Container>
+            <Container size="lg">
+                <Grid>
+                    <GridCol span={6}>
+                        <ArticleCardVertical />
+                    </GridCol>
+                    <GridCol span={6}>
+                        <ArticleCardVertical />
+                    </GridCol>
+                </Grid>
+            </Container>
+        </div>
+
+        <FooterLinks />
+    </div>
+  );
 }
